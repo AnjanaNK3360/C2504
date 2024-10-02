a148908913e20463f4b3b5a0fd24ba2d4dc31849
AnjanaNK3360 <anjanank04@gmail.com>
2024-10-02 18:54:04 +0530

    Add files via upload

---
+++ Phase2-02-10-2024/CounterConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterProject
+{
+    static class CounterConfig
+    {
+        public static CounterWindow Frmcounter {  get; set; }
+        public static CounterViewModel VueModel { get; set; }
+        static CounterConfig()
+        {
+            VueModel = new CounterViewModel();
+            Frmcounter = new CounterWindow();
+        }
+    }
+}
